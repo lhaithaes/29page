e18ae44e288e499a1770c8a64973305b1576fc6c
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-02-07 16:14:36 +0000

    Adjusting the carousel slider options

--- js/carousel-options.js
+++ js/carousel-options.js
@@ -210,7 +210,7 @@ var isOnDiv = false;
         next = $(this).siblings(':first');
       }
       next.children(':first-child').clone().appendTo($(this));
-      for (var i = 1; i < 3; i++) {
+      for (var i = 1; i < 2; i++) {
         next = next.next();
         if (!next.length) {
           next = $(this).siblings(':first');
