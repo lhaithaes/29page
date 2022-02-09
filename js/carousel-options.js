1162dbfcd78872f549cc2d966102ce6cbe9243d7
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-02-09 10:24:17 +0000

    Adjusted the js for the min-height

--- js/carousel-options.js
+++ js/carousel-options.js
@@ -28,33 +28,6 @@
     doAnimations($animatingElems);
   });
 
-  function carouselNormalization() {
-    var items = $('#carousel-fade .item'),
-      heights = [],
-      tallest;
-
-    if (items.length) {
-      function normalizeHeights() {
-        items.each(function() {
-          heights.push($(this).height());
-        });
-        tallest = Math.max.apply(null, heights);
-        items.each(function() {
-          $(this).css('min-height', tallest + 'px');
-        });
-      }
-      normalizeHeights();
-
-      $(window).on('resize orientationchange', function() {
-        tallest = 0, heights.length = 0;
-        items.each(function() {
-          $(this).css('min-height', '0');
-        });
-        normalizeHeights();
-      });
-    }
-  }
-
   function horizontalNormalization() {
     var items = $('.media-carousel .item'),
       heights = [],
@@ -90,6 +63,8 @@
       horizontalNormalization();
     }
   };
+
+  $(document).ready(function() {
 
     $('div[data-ride="carousel"]').each(function() {
       var $carousel = $(this);
