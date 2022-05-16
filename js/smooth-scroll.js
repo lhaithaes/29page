dae9aa0fb91f9bbf9252a1bda30e91e77a075f15
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-05-16 16:39:11 +0100

    Minusing a little off the smooth scroll script

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -19,7 +19,7 @@
             // Only prevent default if animation is actually gonna happen
             event.preventDefault();
             $('html, body').animate({
-              scrollTop: target.offset().top
+              scrollTop: target.offset().top - 150
             }, 1000, function() {
               // Callback after animation
               // Must change focus!
