5f67f5e803c31b94680c94826fcf0fcf4bcdf03b
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-05-17 09:43:23 +0100

    Accidental ( left in causing error

--- js/init-bs.js
+++ js/init-bs.js
@@ -7,7 +7,7 @@
       $(this).tooltip('hide');
     });
 
-    $('a.scroll').on('click', (function(){
+    $('a.scroll').on('click', function(){
       console.log('clicked img');
       $('html, body').animate({
           scrollTop: $('.tab-pane').offset.top - 100
