ff39cdd9241e7c20171e9705ccc62bdfb5a4dda2
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-05-17 10:03:54 +0100

    changing the taregt again fortesting

--- js/init-bs.js
+++ js/init-bs.js
@@ -7,7 +7,7 @@
       $(this).tooltip('hide');
     });
 
-    $('a.scroll img').bind('click', function() {
+    $('.vid-tab').bind('click', function() {
       console.log('clicked img');
       $('html, body').animate({
           scrollTop: $('.tab-pane').offset.top - 100
