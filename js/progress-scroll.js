9d44e94c0a592c32fcbddd741b6d7b902f663d3f
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-05-23 16:56:46 +0100

    acciedental .. removed to just one .

--- js/progress-scroll.js
+++ js/progress-scroll.js
@@ -37,7 +37,7 @@ $(document).ready(function() {
 
   $('.latest-vid').on('click', function(x){
     $('html:not(:animated), body:not(:animated)').animate({
-      scrollTop: $('.tab-pane')..prop("scrollHeight")
+      scrollTop: $('.tab-pane').prop("scrollHeight")
     }, 1000);
     x.preventDefault();
   });
