0873092d9d56e040559aaa6afcdb20d3eb3d5431
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-07-14 09:34:00 +0100

    Testing new js for ht epage to trigger opening on url

---
+++ js/block-reveal.js
@@ -0,0 +1,26 @@
+(function($) {
+  $(document).ready(function() {
+
+    function getUrlVars()
+    {
+      var option = [], tag;
+      var tags = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+      for(var i = 0; i < tags.length; i++)
+      {
+        tag = tags[i].split('=');
+        option.push(tag[0]);
+        option[tag[0]] = tag[1];
+      }
+      return option;
+    }
+
+    var vidString = getUrlVars()["v"];
+    if(vidString == 'rv') {
+      $('#box-set').tirgger('show.bs.tab'){
+        $('html:not(:animated), body:not(:animated)').animate({
+          scrollTop: $('.recent-vid').offset().top - 100
+        }, 1000);
+    }
+  }
+  });
+})(jQuery);
